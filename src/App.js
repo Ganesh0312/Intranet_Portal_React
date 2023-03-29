dc4989c56c153ace37f579eae4a1dc9d00df496b
Ganesh0312 <bobadeganeshd@gmail.com>
2023-03-29 12:05:47 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import EmployeeList from './components/EmployeeList';
+import NewsList from './components/NewsList';
+import ImageList from './components/ImageList';
+import DocumentList from './components/DocumentList';
+
+function App() {
+  return (
+    <div className="container">
+      <ImageList />
+    </div>
+  );
+}
+
+export default App;
